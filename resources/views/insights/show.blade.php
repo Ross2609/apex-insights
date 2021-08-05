2f32c9b4d103826ced621434a746fea739593acd
Ross Mitchell <rossamitchell2609@yahoo.co.uk>
2021-08-05 01:33:25 +0100

    Add Bootstrap Styling

--- resources/views/insights/show.blade.php
+++ resources/views/insights/show.blade.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="{{ Storage::url('js/scripts.js') }}"></script>
-    <title>Insights</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}"></link>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    
+    <title>Apex - Insights</title>
 </head>
 <body></body>
 </html>
